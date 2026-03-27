b3e9b117c422b9463eae12e8f070093129cab487
AndrewPonomare <Andrewponomare@gmail.com>
2026-03-27 22:51:53 +0200

    Update files

---
+++ js/ts/lesson_1/HW-1.2.js
@@ -0,0 +1,7 @@
+let firstName = 'Andrii ';
+let middleName = 'Valeriiovych ';
+let lastName = 'Ponomarenko';
+
+let person = firstName + middleName + lastName;
+
+console.log(person);
---
+++ js/ts/lesson_1/HW-1.4.js
@@ -0,0 +1,5 @@
+let firstName = prompt('Як тебе звати?');
+let middleName = prompt('Яке твоє по батькові?');
+let age = +prompt('Скільки тобі років?');
+
+console.log(`Првиіт ${firstName} ${middleName}, тобі ${age} років.`);
